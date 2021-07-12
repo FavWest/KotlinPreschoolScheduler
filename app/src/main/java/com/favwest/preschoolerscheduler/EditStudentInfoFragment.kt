e51a860e6ee75dc360c48036feafba12bbcea8b9
Tori FavWest <practiceandthenpractice@gmail.com>
2021-07-11 19:19:37 -0700

    WIP: building RecyclerView for EditStudentInfo

--- app/src/main/java/com/favwest/preschoolerscheduler/EditStudentInfoFragment.kt
+++ app/src/main/java/com/favwest/preschoolerscheduler/EditStudentInfoFragment.kt
@@ -6,12 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class EditStudentInfoFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = EditStudentInfoFragment()
-    }
 
     private lateinit var viewModel: EditStudentInfoViewModel
 
@@ -22,8 +20,14 @@ class EditStudentInfoFragment : Fragment() {
         return inflater.inflate(R.layout.edit_student_info_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val studentInfoRecyclerView = view.findViewById<RecyclerView>(R.id.student_info_recycler_view)
+        studentInfoRecyclerView.layoutManager = LinearLayoutManager(context)
+        val tempList = arrayOf("Sam", "Jack", "Lisa")
+        val adapter = EditStudentRecyclerViewAdapter(tempList)
+        studentInfoRecyclerView.adapter = adapter
+
         viewModel = ViewModelProvider(this).get(EditStudentInfoViewModel::class.java)
         // TODO: Use the ViewModel
     }
--- app/src/main/java/com/favwest/preschoolerscheduler/StartFragment.kt
+++ app/src/main/java/com/favwest/preschoolerscheduler/StartFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 
 class StartFragment : Fragment() {
 
@@ -14,5 +16,16 @@ class StartFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_start, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.new_schedule_button).setOnClickListener {
+            this.findNavController().navigate(R.id.action_startFragment_to_newScheduleFragment)
+        }
+        view.findViewById<Button>(R.id.edit_student_info_button).setOnClickListener {
+            this.findNavController().navigate(R.id.action_startFragment_to_editStudentInfoFragment)
+        }
+
     }
 }
